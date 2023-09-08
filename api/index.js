80da980d3ea10c9aeddeeb197f602c772a25f0db
emmanuelniasse <emmanuelniasse@gmail.com>
2023-09-08 10:25:36 +0200

    organize db created

---
+++ api/index.js
@@ -0,0 +1,27 @@
+// LIBRAIRIES
+const express = require('express');
+const app = express();
+const config = require('./config/config.json');
+const router = express.Router();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// DATABASE Connexion
+const dbConnect = require('./router/dbConnect/dbConnect.js');
+dbConnect.connect();
+
+// ROUTERS
+const categoriesRouter = require('./router/categoriesRouter');
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Uniquement "/" car tout est géré dans les routers
+app.use('/', categoriesRouter);
+
+app.listen(config.PORT, () => {
+    console.log('Server listening on port ' + config.PORT);
+});
